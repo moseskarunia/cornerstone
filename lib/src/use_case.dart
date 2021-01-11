3410a0b0ff672489c84db90079f2db586b76253f
Moses Karunia <moseskarunia@gmail.com>
2021-01-11 18:06:53 +0700

    use case

---
+++ lib/src/use_case.dart
@@ -0,0 +1,17 @@
+import 'dart:async';
+
+import 'package:cornerstone/src/failure.dart';
+import 'package:dartz/dartz.dart';
+
+/// Use case contains the app's business logic / rules, e.g. validations.
+/// It should be independent from any other non-business logic-related
+/// functionalities.
+///
+/// It can be connected with repository, although not every use case needs to
+/// depend on the repository.
+/// 
+/// If you can live with less static typing, you can use `Map<String,dynamic`
+/// as Params.
+abstract class UseCase<Type, Params> {
+  FutureOr<Either<Failure, Type>> call(Params params);
+}
