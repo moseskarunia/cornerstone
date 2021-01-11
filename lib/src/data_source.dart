b724500ff4ceee7ef749f34c808a03ff493880bb
Moses Karunia <moseskarunia@gmail.com>
2021-01-11 15:42:48 +0700

    single and multi getter data source

--- lib/src/data_source.dart
+++ lib/src/data_source.dart
@@ -8,22 +8,52 @@ import 'dart:async';
 ///   final String token;
 /// }
 ///
-/// class SessionDataSource implements GetterDataSource<Session, GetParams> {
+/// class SessionDataSource implements
+///   SingleGetterDataSource<Session, GetParams> {
+///
 ///   final HttpClient client;
 ///
 ///   const SessionDataSource({@required this.client});
 ///
 ///   @override
-///   Future<Session> read({GetParams params}) async {
+///   Future<Session> readOne({GetParams params}) async {
 ///     /// Perform client.get() or something.
 ///   }
 /// }
 /// ```
 ///
 /// If you can live with less type safety, you can simply use
-/// `Map<String,dynamic>` as P
-abstract class GetterDataSource<Type, Param> {
-  FutureOr<Type> read({Param param});
+/// `Map<String,dynamic>` as Param
+abstract class SingleGetterDataSource<Type, Param> {
+  FutureOr<Type> readOne({Param param});
+}
+
+/// Implement this abstract in your data source if it can fetch some data.
+/// Note that 1 class can **implements** from multiple abstracts.
+///
+/// ```dart
+/// class GetParams {
+///   final String name;
+/// }
+///
+/// class PeopleDataSource implements
+///   MultipleGetterDataSource<Person, GetParams> {
+///
+///   final HttpClient client;
+///
+///   const PeopleDataSource({@required this.client});
+///
+///   @override
+///   Future<List<Session>> readMany({GetParams params}) async {
+///     /// Perform client.get() or something.
+///   }
+/// }
+/// ```
+///
+/// If you can live with less type safety, you can simply use
+/// `Map<String,dynamic>` as Param
+abstract class MultipleGetterDataSource<Type, Param> {
+  FutureOr<List<Type>> readMany({Param param});
 }
 
 /// Implement this abstract in your data source if it can perform data creation.
@@ -47,7 +77,7 @@ abstract class GetterDataSource<Type, Param> {
 /// ```
 ///
 /// If you can live with less type safety, you can simply use
-/// `Map<String,dynamic>` as P
+/// `Map<String,dynamic>` as Param
 abstract class CreatorDataSource<Type, Param> {
   FutureOr<Type> create({Param param});
 }
