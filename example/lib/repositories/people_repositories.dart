821321930b470e0490d191a56bca8700bde5b5aa
Moses Karunia <moseskarunia@gmail.com>
2021-01-11 23:33:46 +0700

    repo

---
+++ example/lib/repositories/people_repositories.dart
@@ -0,0 +1,25 @@
+import 'package:cornerstone/cornerstone.dart';
+import 'package:dartz/dartz.dart';
+import 'package:example/data_sources/people_data_source.dart';
+import 'package:example/entities/person.dart';
+import 'package:meta/meta.dart';
+
+abstract class PeopleRepository {
+  Future<Either<Failure, List<Person>>> getPeople();
+}
+
+class PeopleRepositoryImpl extends PeopleRepository {
+  final PeopleDataSource dataSource;
+
+  PeopleRepositoryImpl({@required this.dataSource});
+
+  @override
+  Future<Either<Failure, List<Person>>> getPeople() async {
+    try {
+      final results = await dataSource.readMany();
+      return Right(results);
+    } catch (e) {
+      return Left(Failure(name: 'FAILED_TO_RETRIEVE_DATA'));
+    }
+  }
+}
---
+++ example/test/repositories/people_repositories_test.dart
@@ -0,0 +1,59 @@
+import 'package:cornerstone/cornerstone.dart';
+import 'package:dartz/dartz.dart';
+import 'package:example/data_sources/people_data_source.dart';
+import 'package:example/entities/person.dart';
+import 'package:example/repositories/people_repositories.dart';
+import 'package:mockito/mockito.dart';
+import 'package:test/test.dart';
+
+class MockDataSource extends Mock implements PeopleDataSource {}
+
+void main() {
+  final peopleListFixture = [
+    Person(id: '123', name: 'John Doe', email: 'johndoe@test.com'),
+    Person(id: '456', name: 'Tony Stark', email: 'tony@starkindustries.com'),
+  ];
+  MockDataSource dataSource;
+  PeopleRepositoryImpl repo;
+
+  setUp(() {
+    dataSource = MockDataSource();
+    repo = PeopleRepositoryImpl(dataSource: dataSource);
+  });
+
+  group('PeopleRepositoryImpl', () {
+    group('getPeople', () {
+      tearDown(() {
+        verify(dataSource.readMany());
+      });
+      test(
+        'should return Right with list of people',
+        () async {
+          when(dataSource.readMany(param: anyNamed('param'))).thenAnswer(
+            (_) async => peopleListFixture,
+          );
+
+          final result = await repo.getPeople();
+
+          expect((result as Right).value, peopleListFixture);
+        },
+      );
+
+      test(
+        'should return Left with Failure '
+        'when data source throws an exception',
+        () async {
+          when(dataSource.readMany(param: anyNamed('param')))
+              .thenThrow(Exception());
+
+          final result = await repo.getPeople();
+
+          expect(
+            (result as Left).value,
+            Failure(name: 'FAILED_TO_RETRIEVE_DATA'),
+          );
+        },
+      );
+    });
+  });
+}
